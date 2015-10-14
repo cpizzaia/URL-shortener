3ea0d2683f827aa44553e5d446168cf1260ba647
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2015-10-14 11:57:50 -0400

    initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+URLShortener::Application.config.secret_key_base = '4d11aaa48a1182664b862b65e1ca17746722226edbb4d024873ca17a171e155937c534e8618d668d7a1a09ba6d207411355ca10e4a91463985d182ac86af0814'
